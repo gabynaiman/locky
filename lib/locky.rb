284c9620a7739ca26dc9c02d358916598bb333c8
Gabriel Naiman <gabynaiman@gmail.com>
2015-03-05 11:26:37 -0300

    Flexible and strict locks. Forced unlock.

--- lib/locky.rb
+++ lib/locky.rb
@@ -1,34 +1,73 @@
 class Locky
 
-  Error = Class.new StandardError
-  
-  attr_reader :name
+  class Error < StandardError
+    def initialize(locker)
+      @locker = locker
+    end
 
-  def initialize(name, adapter={})
-    @name = name
-    @adapter = adapter
-  end
-
-  def lock(process)
-    raise Error, "#{name} already locked by #{locked_by}" if locked?
-    adapter[name] = process
-    begin
-      yield
-    ensure
-      adapter.delete name
+    def message
+      "#{@locker.name} already locked by #{@locker.locked_by}"
     end
   end
 
+  attr_reader :name
+
+  def initialize(name, storage={})
+    @name = name
+    @storage = storage
+  end
+
+  def lock(process)
+    raise Error, self if locked? && locked_by != process
+    register process
+    begin
+      yield
+    ensure
+      unregister
+    end
+  end
+
+  def lock!(process)
+    raise Error, self if locked?
+    lock(process) { yield }
+  end
+
+  def unlock!
+    storage.delete name
+    storage.delete counter
+  end
+
   def locked?
-    adapter.key? name
+    storage.key? name
   end
 
   def locked_by
-    adapter[name]
+    storage[name]
   end
 
   private
 
-  attr_reader :adapter
+  attr_reader :storage
+
+  def counter
+    "#{name}__counter__"
+  end
+
+  def register(process)
+    if storage.key? name
+      storage[counter] += 1
+    else
+      storage[name] = process
+      storage[counter] = 1
+    end
+  end
+
+  def unregister
+    if storage[counter] > 1
+      storage[counter] -= 1
+    else
+      unlock!
+    end
+  end
 
 end
--- spec/locky_spec.rb
+++ spec/locky_spec.rb
@@ -2,9 +2,10 @@ require 'minitest_helper'
 
 describe Locky do
 
-  let(:locker) { Locky.new :test }
-  
-  it 'Test' do
+  let(:storage) { Hash.new }
+  let(:locker) { Locky.new :test, storage }
+
+  it 'Flexible' do
     locker.name.must_equal :test
     locker.wont_be :locked?
 
@@ -12,10 +13,38 @@ describe Locky do
       locker.must_be :locked?
       locker.locked_by.must_equal :process_1
 
+      locker.lock(:process_1) { }
+
       error = proc { locker.lock :process_2 }.must_raise Locky::Error
       error.message.must_equal 'test already locked by process_1'
     end
 
+    locker.wont_be :locked?
+  end
+
+  it 'Strict' do
+    locker.name.must_equal :test
+    locker.wont_be :locked?
+
+    locker.lock! :process_1 do
+      locker.must_be :locked?
+      locker.locked_by.must_equal :process_1
+
+      error = proc { locker.lock! :process_1 }.must_raise Locky::Error
+      error.message.must_equal 'test already locked by process_1'
+
+      error = proc { locker.lock :process_2 }.must_raise Locky::Error
+      error.message.must_equal 'test already locked by process_1'
+    end
+
+    locker.wont_be :locked?
+  end
+
+  it 'Force unlock' do
+    storage[locker.name] = :process_1
+
+    locker.must_be :locked?
+    locker.unlock!
     locker.wont_be :locked?
   end
 
